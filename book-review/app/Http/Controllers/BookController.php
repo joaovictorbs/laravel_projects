0b8eb7276d4b4475dc70b02407741bd9bb4caa46
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-01-31 23:10:17 -0300

    Cache and Caching Queries

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -29,7 +29,9 @@ class BookController extends Controller
             default => $books->latest()
         };
 
-        $books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
