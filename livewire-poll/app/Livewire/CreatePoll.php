45a82427982455115748156b016e8aa1da249ca8
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-03-19 01:35:18 -0300

    Creating a Poll

--- livewire-poll/app/Livewire/CreatePoll.php
+++ livewire-poll/app/Livewire/CreatePoll.php
@@ -2,12 +2,13 @@
 
 namespace App\Livewire;
 
+use App\Models\Poll;
 use Livewire\Component;
 
 class CreatePoll extends Component
 {
     public $title;
-    public $options = [''];
+    public $options = ['First'];
 
     public function render()
     {
@@ -25,7 +26,18 @@ class CreatePoll extends Component
         $this->options = array_values($this->options);
     }
 
+    public function createPoll()
+    {
+        $poll = Poll::create([
+            'title' => $this->title
+        ]);
 
+        foreach($this->options as $optionName) {
+            $poll->options()->create(['name' => $optionName]);
+        }
+
+        $this->reset(['title', 'options']);
+    }
     // public function mount()
     // {
 
--- livewire-poll/resources/views/livewire/create-poll.blade.php
+++ livewire-poll/resources/views/livewire/create-poll.blade.php
@@ -1,10 +1,10 @@
 <div>
-    <form>
+    <form wire:submit.prevent="createPoll">
         <label>Poll Title</label>
 
         <input type="text" wire:model.live="title" />
 
-        Current title: {{ $title }}
+        {{-- Current title: {{ $title }} --}}
 
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
@@ -22,5 +22,7 @@
 
             @endforeach
         </div>
+
+        <button type="submit" class="btn">Create Poll</button>
     </form>
 </div>
