e23861e16386c6e51731b3ddcf1f6114091c47cc
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-03-19 00:53:57 -0300

    Actions in Livewire

--- livewire-poll/app/Livewire/CreatePoll.php
+++ livewire-poll/app/Livewire/CreatePoll.php
@@ -7,9 +7,20 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = [''];
 
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    // public function mount()
+    // {
+
+    // }
 }
