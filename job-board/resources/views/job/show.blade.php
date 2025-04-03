47c2eca4585873c7c3ee21ad138b9df6d1c08974
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-04-03 14:42:00 -0300

    Applying for Jobs: Did I Already Apply?

--- job-board/resources/views/job/show.blade.php
+++ job-board/resources/views/job/show.blade.php
@@ -6,9 +6,15 @@
             {!! nl2br(e($job->description)) !!}
         </p>
 
-        <x-link-button :href="route('job.application.create', $job)">
-            Apply
-        </x-link-button>
+        @can('apply', $job)
+            <x-link-button :href="route('job.application.create', $job)">
+                Apply
+            </x-link-button>
+        @else
+            <div class="text-center text-sm font-medium text-slate-500">
+                You already applied to this job
+            </div>
+        @endcan
     </x-job-card>
 
     <x-card class="mb-4">
@@ -18,21 +24,21 @@
 
         <div class="text-sm text-slate-500">
             @foreach ($job->employer->jobs as $otherJob)
-                <div class="mb-4 flex justify-between">
-                    <div>
-                        <div class="text-slate-700">
-                            <a href="{{ route('jobs.show', $otherJob)}}">
-                                {{ $otherJob->title }}
-                            </a>
-                        </div>
-                        <div class="text-xs">
-                            {{ $otherJob->created_at->diffForHumans() }}
-                        </div>
-                    </div>
-                    <div class="text-xs">
-                        ${{ number_format($otherJob->salary) }}
-                    </div>
+              <div class="mb-4 flex justify-between">
+                <div>
+                  <div class="text-slate-700">
+                    <a href="{{ route('jobs.show', $otherJob) }}">
+                      {{ $otherJob->title }}
+                    </a>
+                  </div>
+                  <div class="text-xs">
+                    {{ $otherJob->created_at->diffForHumans() }}
+                  </div>
                 </div>
+                <div class="text-xs">
+                  ${{ number_format($otherJob->salary) }}
+                </div>
+              </div>
             @endforeach
         </div>
     </x-card>
