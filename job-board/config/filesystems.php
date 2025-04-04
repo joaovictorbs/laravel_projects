b466e54ae085f4c041ebd7f31143f428df103ae6
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-04-03 21:46:10 -0300

    File Uploads: Uploading Files

--- job-board/config/filesystems.php
+++ job-board/config/filesystems.php
@@ -50,7 +50,7 @@ return [
         'private' => [
             'driver' => 'local',
             'root' => storage_path('app/private'),
-            'visibility' => 'private'
+            'visibility' => 'private',
         ],
 
         's3' => [
