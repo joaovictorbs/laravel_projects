74ee782657886b9d7f2d233b17a08202d53b3db8
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-04-18 02:11:56 -0300

    Conditionals - If Statement

---
+++ phpCrashCourse/if.php
@@ -0,0 +1,52 @@
+<?php
+
+$x = 10;
+
+if ($x > 5) {
+    echo "x is greater than 5\n";
+}
+
+
+$score = 85;
+
+if ($score >= 90) {
+    echo "A";
+}
+elseif ($score >= 80) {
+    echo "B";
+}
+elseif ($score >= 70) {
+    echo "C";
+}
+elseif ($score >= 60) {
+    echo "D";
+}
+else {
+    echo "F";
+}
+
+
+$num = -3;
+
+if ($num > 0) {
+    if ($num % 2 == 0) {
+        echo "\nPositive even number";
+    }
+    else {
+        echo "\nPositive odd number";
+    }
+}
+else {
+    echo "\nNon-positive number\n";
+}
+
+
+$username = "admin";
+$password = "password123";
+
+if ($username == "admin" && $password == "password123") {
+    echo "Success";
+}
+else {
+    echo "Failure";
+}
