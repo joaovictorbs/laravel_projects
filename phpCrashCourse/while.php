a4b09acd0944307216616bc43792ccacb959f868
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-04-18 18:25:33 -0300

    Loops - While Loop (Guess the Secret Game)

---
+++ phpCrashCourse/while.php
@@ -0,0 +1,22 @@
+<?php
+
+$secret = "magic";
+$attempts = 0;
+$maxAttemps = 5;
+
+while ($attempts < $maxAttemps) {
+    echo "Guess the password: ";
+    $guess = trim(fgets(STDIN));
+    $attempts++;
+
+    if ($guess == $secret) {
+        echo "Correct! You've unlocked the treasure!\n";
+        break;
+    }
+    elseif ($attempts == $maxAttemps) {
+        echo "Out of attempts! The treasure remains locked.\n";
+    }
+    else {
+        echo "Wrong! Try Again. Attempts left: " . ($maxAttemps - $attempts) . "\n";
+    }
+}
