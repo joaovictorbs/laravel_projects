83f132f291a08bedd88503dd948d4ae92e79906e
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-04-20 14:07:28 -0300

    Anonymous Functions

---
+++ phpCrashCourse/functions/anon.php
@@ -0,0 +1,25 @@
+<?php
+
+$greet = function ($name) {
+    return "Hello, $name\n";
+};
+
+echo $greet("Joao");
+
+
+$numbers = [1, 2, 3];
+$squared = array_map(function ($n) {
+    return $n * $n;
+}, $numbers);
+
+var_dump($numbers, $squared);
+
+
+$message = "Bye";
+$greet2 = function ($name) use (&$message) {
+    $message = $message . "!";
+    return "$message, $name\n";
+};
+
+echo $greet2("Joao");
+echo $message . "\n";
